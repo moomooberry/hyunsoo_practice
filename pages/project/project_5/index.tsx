be4eed4674fc8d8c773afffb93177d89ce07b244
moo <106214654+moomooberry@users.noreply.github.com>
2022-12-21 18:27:25 +0900

    auth 관련 req.cookie로 Token_2
    빼먹은거

--- pages/project/project_5/index.tsx
+++ pages/project/project_5/index.tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps } from "next";
 import ControllerFive from "../../../screens/project_5_useFieldArray/Project5Controller";
 
 const ProjectFive = () => {
@@ -5,3 +6,13 @@ const ProjectFive = () => {
 };
 
 export default ProjectFive;
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const token = req.cookies["moo-token"];
+  //token으로 뭔갈 하는 함수를 작성한 뒤 return 값을 props로 넘겨줄 예정
+
+  if (!token) return { redirect: { destination: "/", permanent: false } };
+  return {
+    props: {},
+  };
+};
