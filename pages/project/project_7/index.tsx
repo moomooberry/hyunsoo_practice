21912438a30bad824d13f8bec655e419a665bcfa
moo <106214654+moomooberry@users.noreply.github.com>
2022-12-21 18:30:44 +0900

    auth 관련 req.cookie로 Token_3
    빼먹은거 2 ㅋㅋ

--- pages/project/project_7/index.tsx
+++ pages/project/project_7/index.tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps } from "next";
 import ControllerSeven from "../../../screens/project_7_useQuery_get_delete/Project7Controller";
 
 const ProjectSeven = () => {
@@ -5,3 +6,11 @@ const ProjectSeven = () => {
 };
 
 export default ProjectSeven;
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const token = req.cookies["moo-token"];
+  if (!token) return { redirect: { destination: "/", permanent: false } };
+  return {
+    props: {},
+  };
+};
